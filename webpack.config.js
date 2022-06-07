2749521acf2793bb396b28d2c268b196e8094689
yuval <nhajeho1@gmail.com>
2022-06-07 15:11:24 +0300

    config project for html deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  mode: 'development',
-  entry: "./dist/tsc/app.js",
-  devtool: "source-map",
-  output: {
-    filename: "app.js",
-    library: "app"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: "pre",
-        use: ["source-map-loader"],
-      },
-    ],
-  }
-}
+	mode: "development",
+	entry: "./dist/tsc/src/app.js",
+	devtool: "source-map",
+	output: {
+		filename: "app.js",
+		library: "app",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				enforce: "pre",
+				use: ["source-map-loader"],
+			},
+		],
+	},
+};
