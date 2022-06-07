3e667f0136b2f48721e12d57621b3324ac24c033
yuval <nhajeho1@gmail.com>
2022-06-07 13:41:11 +0300

    made app run game with dummy renderer

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
 import { Ball } from "./ball";
+import { Game } from "./game";
 import { Paddle } from "./paddle";
 import { dummyRenderer, objTypes, Renderer } from "./renderer";
 import { vector } from "./vector";
+import { Box } from "./helpers";
 
-const paddle = new Paddle(
-	new vector(0, 0),
-	2,
+const field: Box = [new vector(0, 0), new vector(100, 100)];
+const renderer = new dummyRenderer();
+
+const ball = new Ball(field[1].div(2), field, renderer);
+const lPaddle = new Paddle(
+	new vector(0, field[1].y / 2),
+	5,
 	1,
-	2,
-	"right",
-	[new vector(0, 0), new vector(10, 10)],
-	new dummyRenderer()
+	3,
+	"lPaddle",
+	field,
+	renderer
 );
-
-paddle.position = new vector(1, 1);
-
-paddle.moveY(3);
-paddle.moveY(-3);
+const rPaddle = new Paddle(
+	new vector(field[1].x, field[1].y / 2),
+	5,
+	1,
+	3,
+	"rPaddle",
+	field,
+	renderer
+);
+const game = new Game(ball, lPaddle, rPaddle, renderer);
+game.start((1 / 60) * 1000);
