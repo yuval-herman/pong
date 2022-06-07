4eb032aa518b2ebac148f7c7ce76dc3dfcc6b43f
yuval <nhajeho1@gmail.com>
2022-06-07 07:44:21 +0300

    added mini test for Ball in app.ts

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { Ball } from "./ball";
+import { objTypes, Renderer } from "./renderer";
+import { vector } from "./vector";
+
+class dummyRenderer implements Renderer {
+	objects: Object = {};
+	move(name: string, position: vector): void {
+		console.log("move", name, position);
+	}
+	make(name: string, type: objTypes, height: number, width: number): void {
+		console.log("make", name, type, height, width);
+	}
+}
+
+const ball = new Ball(
+	new vector(0, 0),
+	[new vector(0, 0), new vector(10, 10)],
+	new dummyRenderer()
+);
+
+ball.moveByForce(new vector(1,1))
