c47817e565e2b67eabc51236154753b8616ed2f2
yuval <nhajeho1@gmail.com>
2022-06-07 07:43:36 +0300

    repaired Ball position property

--- src/ball.ts
+++ src/ball.ts
@@ -1,14 +1,15 @@
 import { box, isInside } from "./helpers";
-import { objTypes, renderer as Renderer } from "./renderer";
+import { objTypes, Renderer as Renderer } from "./renderer";
 import { vector } from "./vector";
 
 export class Ball {
+	private _position:vector;
 	renderer: Renderer;
 	dirForce: vector;
     box:[vector,vector];
     radius: number
 	constructor(position: vector, box:box, renderer: Renderer) {
-		this.position = position;
+		this._position = position;
 		this.dirForce = new vector(0, 0);
 		this.renderer = renderer;
         this.box = box;
@@ -17,24 +18,18 @@ export class Ball {
 	}
 
 	public get position(): vector {
-		return this.position;
+		return this._position;
 	}
 
 	public set position(v: vector) {
-		this.position = v;
-		this.renderer.move("ball", this.position);
+		this._position = v;
+		this.renderer.move("ball", this._position);
 	}
 
-    handleCollision() {
-
-    }
-
-    moveByForce() {
-        const newPos = this.position.add(this.dirForce);
+    moveByForce(dirForce:vector = this.dirForce) {
+        const newPos = this.position.add(dirForce);
         if (isInside(newPos, this.box)) {
             this.position = newPos;
-        } else {
-            this.handleCollision()
         }
     }
 
