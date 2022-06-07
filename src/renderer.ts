f91053e3249e2e9423d2843d361339bcabc7201d
yuval <nhajeho1@gmail.com>
2022-06-07 13:40:38 +0300

    added interval method to renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -9,14 +9,23 @@ export interface Renderer {
 	objects: Object;
 	make(name: string, type: objTypes, height: number, width: number): void;
 	move(name: string, position: vector): void;
+	setInterval(fn: Function, ms: number): void;
+	stopInterval(): void;
 }
 
 export class dummyRenderer implements Renderer {
 	objects: Object = {};
+	intervalId: number = 0;
 	move(name: string, position: vector): void {
 		console.log("move", name, position);
 	}
 	make(name: string, type: objTypes, height: number, width: number): void {
 		console.log("make", name, type, height, width);
+	}
+	setInterval(fn: Function, ms: number) {
+		this.intervalId = setInterval(fn, ms);
+	}
+	stopInterval() {
+		clearInterval(this.intervalId);
 	}
 }
